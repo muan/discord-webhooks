dd84ef186c632f5afb515fafef12530f3b2b3907
Mu-An Chiou <me@muanchiou.com>
2021-03-18 15:28:50 +0800

    Add basics

---
+++ README.md
@@ -0,0 +1,5 @@
+# Discord Webhooks
+
+Parse Heroku deploy payload.
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fmuan%2Fdiscord-webhooks)
---
+++ api/heroku.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch')
+module.exports = async function(req, res) {
+  if (!req.query.discord) {
+    return res.end()
+  }
+  const body = {
+    embeds: [{
+      title: `[${req.body.data.app.name}] ${req.body.data.description}`,
+      description: req.body.data.slug.commit_description,
+      color: 7419530
+    }]
+  }
+  await postTo(req.query.discord, body)
+  res.end()
+}
+
+async function postTo(url, body) {
+  return fetch(url, {
+    method: 'POST',
+    body:    JSON.stringify(body),
+    headers: {'Content-Type': 'application/json'}
+  })
+}
